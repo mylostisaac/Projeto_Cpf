fec528f1e44b05f722164b4e06d7133244ee253c
Lucas Daniel Tomko <lucasrtg11@gmail.com>
2018-10-10 01:22:03 -0300

    aaa

--- cpf/urls.py
+++ cpf/urls.py
@@ -8,8 +8,6 @@ urlpatterns = [
 
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('vote/', views.vote, name='vote'),
     path('<int:question_id>/voltar/', views.voltar, name='voltar'),
     path('validate', views.validateCpf, name='validate'),
     path('generate', views.generateCpf, name='generate'),
